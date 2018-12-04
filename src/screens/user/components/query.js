a9821138f6453835366a12042332e780e80e1a77
Sam Zhao <zhao.shengjun@gmail.com>
2018-12-04 22:08:24 +1000

    replace prop types and default props

--- src/screens/user/components/query.js
+++ src/screens/user/components/query.js
@@ -3,18 +3,16 @@ import PropTypes from "prop-types";
 import isEqual from "lodash/isEqual";
 import * as GitHub from "../../../github-client";
 
+function Query({ query, variables, children, normalize = data => data }) {}
+
+Query.porpTypes = {
+  query: PropTypes.string.isRequired,
+  variables: PropTypes.object,
+  children: PropTypes.func.isRequired,
+  normalize: PropTypes.func
+};
+
 class Query extends Component {
-  static porpTypes = {
-    query: PropTypes.string.isRequired,
-    variables: PropTypes.object,
-    children: PropTypes.func.isRequired,
-    normalize: PropTypes.func
-  };
-
-  static defaultProps = {
-    normalize: data => data
-  };
-
   static contextType = GitHub.Context;
 
   state = { loaded: false, fetching: false, data: null, error: null };
